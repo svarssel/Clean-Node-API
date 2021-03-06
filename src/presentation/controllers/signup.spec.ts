2b90d88f2bc7b4d8d3fbd95b1529d55b031969c7
Fabiano Melo <svarssel@gmail.com>
2021-03-05 22:00:30 -0300

    refactor: ensure SignUpController.handle

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,6 @@
 import {SignUpController} from './signup'
 
+
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
       const sut = new SignUpController()
